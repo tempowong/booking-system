46763e22ac5424b1511531bfc9f637b1ec5b3b28
tempowong <tempowong28@gmail.com>
2022-12-11 13:32:40 +0800

    commit

--- src/pages/ecgoathome.tsx
+++ src/pages/ecgoathome.tsx
@@ -1,7 +1,7 @@
 import Section1 from "@component/ecgoat/Section1";
 import Section2 from "@component/ecgoat/Section2";
 import Section4 from "@component/ecgoat/Section4";
-import Section5 from "@component/ecgoat/Section5";
+// import Section5 from "@component/ecgoat/Section5";
 
 import { Fragment } from "react";
 import AppLayout from "../components/layout/AppLayout";
@@ -12,7 +12,7 @@ const ECGIndexPage = () => {
             <Section1 />
             <Section2 />
             <Section4 />
-            <Section5 />
+            {/* <Section5 /> */}
         </Fragment>
     );
 };
